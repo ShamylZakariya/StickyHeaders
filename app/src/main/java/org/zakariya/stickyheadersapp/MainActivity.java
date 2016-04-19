efcb772d20c066d107ecc5a6b8a3e8a929ac8088
Shamyl Zakariya <shamyl@gmail.com>
2016-04-19 07:51:23 -0700

    Demo app now has a view pager to switch between 2 basic demo scenarios

--- app/src/main/java/org/zakariya/stickyheadersapp/MainActivity.java
+++ app/src/main/java/org/zakariya/stickyheadersapp/MainActivity.java
@@ -1,13 +1,16 @@
 package org.zakariya.stickyheadersapp;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,30 +18,121 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.TextView;
 
-import org.zakariya.stickyheaders.SectioningAdapter;
 import org.zakariya.stickyheaders.StickyHeaderLayoutManager;
-
-import java.util.ArrayList;
+import org.zakariya.stickyheadersapp.adapters.SimpleDemoAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
 	private static final String TAG = "MainActivity";
-	private static final boolean useStickyHeadersLayoutManager = true;
-	private static final boolean useDebugColoration = false;
 
-	RecyclerView recyclerView;
+	TabLayout tabs;
+	ViewPager viewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+		tabs = (TabLayout) findViewById(R.id.tabs);
+		viewPager = (ViewPager) findViewById(R.id.viewPager);
+		viewPager.setAdapter(new RecyclerViewsPager(getSupportFragmentManager()));
+		tabs.setupWithViewPager(viewPager);
+	}
 
-		if (useStickyHeadersLayoutManager) {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu_activity_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private static class RecyclerViewsPager extends FragmentPagerAdapter {
+
+		public RecyclerViewsPager(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			switch(position) {
+				case 0: return new SectionsDemo();
+				case 1: return new StickySectionHeadersDemo();
+				default: return null;
+			}
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+				case 0: return SectionsDemo.demoName();
+				case 1: return StickySectionHeadersDemo.demoName();
+				default: return null;
+			}
+		}
+
+		@Override
+		public int getCount() {
+			return 2;
+		}
+	}
+
+	public static class RecyclerViewFragment extends Fragment {
+
+		RecyclerView recyclerView;
+
+		public RecyclerViewFragment() {}
+
+		@Nullable
+		@Override
+		public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+			View view = inflater.inflate(R.layout.fragment_recycler, container, false);
+			recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+			return view;
+		}
+	}
+
+	public static class SectionsDemo extends RecyclerViewFragment {
+		public SectionsDemo() {
+		}
+
+		public static String demoName() {
+			return "Sections";
+		}
+
+		@Nullable
+		@Override
+		public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+			View v = super.onCreateView(inflater, container, savedInstanceState);
+
+			recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+			recyclerView.setAdapter(new SimpleDemoAdapter(5, 5));
+
+			return v;
+		}
+	}
+
+	public static class StickySectionHeadersDemo extends RecyclerViewFragment {
+		public StickySectionHeadersDemo() {
+		}
+
+		public static String demoName() {
+			return "Sticky Section Headers";
+		}
+
+		@Nullable
+		@Override
+		public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+			View v = super.onCreateView(inflater, container, savedInstanceState);
+
 			StickyHeaderLayoutManager stickyHeaderLayoutManager = new StickyHeaderLayoutManager();
 			recyclerView.setLayoutManager(stickyHeaderLayoutManager);
 
@@ -54,263 +148,11 @@ public class MainActivity extends AppCompatActivity {
 				}
 			});
 
-		} else {
-			recyclerView.setLayoutManager(new LinearLayoutManager(this));
-		}
+			recyclerView.setLayoutManager(stickyHeaderLayoutManager);
+			recyclerView.setAdapter(new SimpleDemoAdapter(5, 5));
 
-		recyclerView.setAdapter(new DemoAdapter(this));
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.menu_activity_main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.reloadMenuItem:
-				((SectioningAdapter) recyclerView.getAdapter()).notifyAllSectionsDataSetChanged();
-				return true;
-
-			default:
-				return super.onOptionsItemSelected(item);
+			return v;
 		}
 	}
 
-	private static final class Section {
-		int index;
-		int copyCount;
-		String header;
-		String footer;
-		ArrayList<String> items = new ArrayList<>();
-
-		public Section duplicate() {
-			Section c = new Section();
-			c.index = this.index;
-			c.copyCount = this.copyCount + 1;
-			c.header = c.index + " copy " + c.copyCount;
-			c.footer = this.footer;
-			for (String i : this.items) {
-				c.items.add(i + " copy " + c.copyCount);
-			}
-
-			return c;
-		}
-
-		public void duplicateItem(int item) {
-			String itemCopy = items.get(item) + " copy";
-			items.add(item + 1, itemCopy);
-		}
-
-	}
-
-	public class DemoAdapter extends SectioningAdapter {
-
-		public class ItemViewHolder extends SectioningAdapter.ItemViewHolder implements View.OnClickListener {
-			TextView textView;
-			TextView adapterPositionTextView;
-			ImageButton cloneButton;
-			ImageButton deleteButton;
-
-			public ItemViewHolder(View itemView) {
-				super(itemView);
-				textView = (TextView) itemView.findViewById(R.id.textView);
-				adapterPositionTextView = (TextView) itemView.findViewById(R.id.adapterPositionTextView);
-				cloneButton = (ImageButton) itemView.findViewById(R.id.cloneImageButton);
-				cloneButton.setOnClickListener(this);
-				deleteButton = (ImageButton) itemView.findViewById(R.id.deleteImageButton);
-				deleteButton.setOnClickListener(this);
-			}
-
-			@Override
-			public void onClick(View v) {
-				int adapterPosition = getAdapterPosition();
-				final int section = DemoAdapter.this.getSectionForAdapterPosition(adapterPosition);
-				final int item = DemoAdapter.this.getPositionOfItemInSection(section, adapterPosition);
-				if (v == cloneButton) {
-					DemoAdapter.this.onCloneItem(section, item);
-				} else if (v == deleteButton) {
-					DemoAdapter.this.onDeleteItem(section, item);
-				}
-			}
-		}
-
-		public class HeaderViewHolder extends SectioningAdapter.HeaderViewHolder implements View.OnClickListener {
-			TextView textView;
-			TextView adapterPositionTextView;
-			ImageButton cloneButton;
-			ImageButton deleteButton;
-
-			public HeaderViewHolder(View itemView) {
-				super(itemView);
-				textView = (TextView) itemView.findViewById(R.id.textView);
-				adapterPositionTextView = (TextView) itemView.findViewById(R.id.adapterPositionTextView);
-				cloneButton = (ImageButton) itemView.findViewById(R.id.cloneImageButton);
-				cloneButton.setOnClickListener(this);
-				deleteButton = (ImageButton) itemView.findViewById(R.id.deleteImageButton);
-				deleteButton.setOnClickListener(this);
-			}
-
-			@Override
-			public void onClick(View v) {
-				int position = getAdapterPosition();
-				final int section = DemoAdapter.this.getSectionForAdapterPosition(position);
-				if (v == cloneButton) {
-					DemoAdapter.this.onCloneSection(section);
-				} else if (v == deleteButton) {
-					DemoAdapter.this.onDeleteSection(section);
-				}
-
-			}
-		}
-
-		public class FooterViewHolder extends SectioningAdapter.FooterViewHolder {
-			TextView textView;
-			TextView adapterPositionTextView;
-
-			public FooterViewHolder(View itemView) {
-				super(itemView);
-				textView = (TextView) itemView.findViewById(R.id.textView);
-				adapterPositionTextView = (TextView) itemView.findViewById(R.id.adapterPositionTextView);
-			}
-		}
-
-
-		Context context;
-		ArrayList<Section> sections = new ArrayList<>();
-
-		public DemoAdapter(Context context) {
-			this.context = context;
-
-			for (int i = 0; i < 25; i++) {
-				appendSection(i, 25);
-			}
-		}
-
-		void appendSection(int index, int itemCount) {
-			Section section = new Section();
-			section.index = index;
-			section.copyCount = 0;
-			section.header = Integer.toString(index);
-			section.footer = "End of section " + index;
-
-			for (int j = 0; j < itemCount; j++) {
-				section.items.add(index + "/" + j);
-			}
-
-			sections.add(section);
-		}
-
-		void onDeleteSection(int sectionIndex) {
-			Log.d(TAG, "onDeleteSection() called with: " + "sectionIndex = [" + sectionIndex + "]");
-			sections.remove(sectionIndex);
-			notifySectionRemoved(sectionIndex);
-		}
-
-		void onCloneSection(int sectionIndex) {
-			Log.d(TAG, "onCloneSection() called with: " + "sectionIndex = [" + sectionIndex + "]");
-
-			Section s = sections.get(sectionIndex);
-			Section d = s.duplicate();
-			sections.add(sectionIndex + 1, d);
-			notifySectionInserted(sectionIndex + 1);
-		}
-
-		void onDeleteItem(int sectionIndex, int itemIndex) {
-			Log.d(TAG, "onDeleteItem() called with: " + "sectionIndex = [" + sectionIndex + "], itemIndex = [" + itemIndex + "]");
-			Section s = sections.get(sectionIndex);
-			s.items.remove(itemIndex);
-			notifySectionItemRemoved(sectionIndex, itemIndex);
-		}
-
-		void onCloneItem(int sectionIndex, int itemIndex) {
-			Log.d(TAG, "onCloneItem() called with: " + "sectionIndex = [" + sectionIndex + "], itemIndex = [" + itemIndex + "]");
-			Section s = sections.get(sectionIndex);
-			s.duplicateItem(itemIndex);
-			notifySectionItemInserted(sectionIndex, itemIndex + 1);
-		}
-
-		@Override
-		public int getNumberOfSections() {
-			return sections.size();
-		}
-
-		@Override
-		public int getNumberOfItemsInSection(int sectionIndex) {
-			return sections.get(sectionIndex).items.size();
-		}
-
-		@Override
-		public boolean doesSectionHaveHeader(int sectionIndex) {
-			return !TextUtils.isEmpty(sections.get(sectionIndex).header);
-		}
-
-		@Override
-		public boolean doesSectionHaveFooter(int sectionIndex) {
-			return !TextUtils.isEmpty(sections.get(sectionIndex).footer);
-		}
-
-		@Override
-		public ItemViewHolder onCreateItemViewHolder(ViewGroup parent) {
-			LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-			View v = inflater.inflate(R.layout.list_item, parent, false);
-			return new ItemViewHolder(v);
-		}
-
-		@Override
-		public HeaderViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
-			LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-			View v = inflater.inflate(R.layout.list_item_header, parent, false);
-			return new HeaderViewHolder(v);
-		}
-
-		@Override
-		public FooterViewHolder onCreateFooterViewHolder(ViewGroup parent) {
-			LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-			View v = inflater.inflate(R.layout.list_item_footer, parent, false);
-			return new FooterViewHolder(v);
-		}
-
-		@SuppressLint("SetTextI18n")
-		@Override
-		public void onBindItemViewHolder(SectioningAdapter.ItemViewHolder viewHolder, int sectionIndex, int itemIndex) {
-			Section s = sections.get(sectionIndex);
-			ItemViewHolder ivh = (ItemViewHolder) viewHolder;
-			ivh.textView.setText(s.items.get(itemIndex));
-			ivh.adapterPositionTextView.setText(Integer.toString(getAdapterPositionForSectionItem(sectionIndex, itemIndex)));
-		}
-
-		@SuppressLint("SetTextI18n")
-		@Override
-		public void onBindHeaderViewHolder(SectioningAdapter.HeaderViewHolder viewHolder, int sectionIndex) {
-			Section s = sections.get(sectionIndex);
-			HeaderViewHolder hvh = (HeaderViewHolder) viewHolder;
-			hvh.textView.setText(s.header);
-			hvh.adapterPositionTextView.setText(Integer.toString(getAdapterPositionForSectionHeader(sectionIndex)));
-
-			if (useDebugColoration) {
-				viewHolder.itemView.setBackgroundColor(0x55FF9999);
-			}
-		}
-
-		@Override
-		public void onBindGhostHeaderViewHolder(SectioningAdapter.GhostHeaderViewHolder viewHolder, int sectionIndex) {
-			if (useDebugColoration) {
-				viewHolder.itemView.setBackgroundColor(0xFF9999FF);
-			}
-		}
-
-		@SuppressLint("SetTextI18n")
-		@Override
-		public void onBindFooterViewHolder(SectioningAdapter.FooterViewHolder viewHolder, int sectionIndex) {
-			Section s = sections.get(sectionIndex);
-			FooterViewHolder fvh = (FooterViewHolder) viewHolder;
-			fvh.textView.setText(s.footer);
-			fvh.adapterPositionTextView.setText(Integer.toString(getAdapterPositionForSectionFooter(sectionIndex)));
-		}
-
-	}
 }
---
+++ app/src/main/java/org/zakariya/stickyheadersapp/adapters/SimpleDemoAdapter.java
@@ -0,0 +1,253 @@
+package org.zakariya.stickyheadersapp.adapters;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.zakariya.stickyheaders.SectioningAdapter;
+import org.zakariya.stickyheadersapp.R;
+
+import java.util.ArrayList;
+
+/**
+ * SimpleDemoAdapter
+ */
+public class SimpleDemoAdapter extends SectioningAdapter {
+
+	private static final String TAG = SimpleDemoAdapter.class.getSimpleName();
+	private static final boolean useDebugColoration = false;
+
+	private class Section {
+		int index;
+		int copyCount;
+		String header;
+		String footer;
+		ArrayList<String> items = new ArrayList<>();
+
+		public Section duplicate() {
+			Section c = new Section();
+			c.index = this.index;
+			c.copyCount = this.copyCount + 1;
+			c.header = c.index + " copy " + c.copyCount;
+			c.footer = this.footer;
+			for (String i : this.items) {
+				c.items.add(i + " copy " + c.copyCount);
+			}
+
+			return c;
+		}
+
+		public void duplicateItem(int item) {
+			String itemCopy = items.get(item) + " copy";
+			items.add(item + 1, itemCopy);
+		}
+
+	}
+
+	public class ItemViewHolder extends SectioningAdapter.ItemViewHolder implements View.OnClickListener {
+		TextView textView;
+		TextView adapterPositionTextView;
+		ImageButton cloneButton;
+		ImageButton deleteButton;
+
+		public ItemViewHolder(View itemView) {
+			super(itemView);
+			textView = (TextView) itemView.findViewById(R.id.textView);
+			adapterPositionTextView = (TextView) itemView.findViewById(R.id.adapterPositionTextView);
+			cloneButton = (ImageButton) itemView.findViewById(R.id.cloneImageButton);
+			cloneButton.setOnClickListener(this);
+			deleteButton = (ImageButton) itemView.findViewById(R.id.deleteImageButton);
+			deleteButton.setOnClickListener(this);
+		}
+
+		@Override
+		public void onClick(View v) {
+			int adapterPosition = getAdapterPosition();
+			final int section = SimpleDemoAdapter.this.getSectionForAdapterPosition(adapterPosition);
+			final int item = SimpleDemoAdapter.this.getPositionOfItemInSection(section, adapterPosition);
+			if (v == cloneButton) {
+				SimpleDemoAdapter.this.onCloneItem(section, item);
+			} else if (v == deleteButton) {
+				SimpleDemoAdapter.this.onDeleteItem(section, item);
+			}
+		}
+	}
+
+	public class HeaderViewHolder extends SectioningAdapter.HeaderViewHolder implements View.OnClickListener {
+		TextView textView;
+		TextView adapterPositionTextView;
+		ImageButton cloneButton;
+		ImageButton deleteButton;
+
+		public HeaderViewHolder(View itemView) {
+			super(itemView);
+			textView = (TextView) itemView.findViewById(R.id.textView);
+			adapterPositionTextView = (TextView) itemView.findViewById(R.id.adapterPositionTextView);
+			cloneButton = (ImageButton) itemView.findViewById(R.id.cloneImageButton);
+			cloneButton.setOnClickListener(this);
+			deleteButton = (ImageButton) itemView.findViewById(R.id.deleteImageButton);
+			deleteButton.setOnClickListener(this);
+		}
+
+		@Override
+		public void onClick(View v) {
+			int position = getAdapterPosition();
+			final int section = SimpleDemoAdapter.this.getSectionForAdapterPosition(position);
+			if (v == cloneButton) {
+				SimpleDemoAdapter.this.onCloneSection(section);
+			} else if (v == deleteButton) {
+				SimpleDemoAdapter.this.onDeleteSection(section);
+			}
+
+		}
+	}
+
+	public class FooterViewHolder extends SectioningAdapter.FooterViewHolder {
+		TextView textView;
+		TextView adapterPositionTextView;
+
+		public FooterViewHolder(View itemView) {
+			super(itemView);
+			textView = (TextView) itemView.findViewById(R.id.textView);
+			adapterPositionTextView = (TextView) itemView.findViewById(R.id.adapterPositionTextView);
+		}
+	}
+
+
+	ArrayList<Section> sections = new ArrayList<>();
+
+	public SimpleDemoAdapter(int numSections, int numItemsPerSection) {
+		for (int i = 0; i < numSections; i++) {
+			appendSection(i, numItemsPerSection);
+		}
+	}
+
+	void appendSection(int index, int itemCount) {
+		Section section = new Section();
+		section.index = index;
+		section.copyCount = 0;
+		section.header = Integer.toString(index);
+		section.footer = "End of section " + index;
+
+		for (int j = 0; j < itemCount; j++) {
+			section.items.add(index + "/" + j);
+		}
+
+		sections.add(section);
+	}
+
+	void onDeleteSection(int sectionIndex) {
+		Log.d(TAG, "onDeleteSection() called with: " + "sectionIndex = [" + sectionIndex + "]");
+		sections.remove(sectionIndex);
+		notifySectionRemoved(sectionIndex);
+	}
+
+	void onCloneSection(int sectionIndex) {
+		Log.d(TAG, "onCloneSection() called with: " + "sectionIndex = [" + sectionIndex + "]");
+
+		Section s = sections.get(sectionIndex);
+		Section d = s.duplicate();
+		sections.add(sectionIndex + 1, d);
+		notifySectionInserted(sectionIndex + 1);
+	}
+
+	void onDeleteItem(int sectionIndex, int itemIndex) {
+		Log.d(TAG, "onDeleteItem() called with: " + "sectionIndex = [" + sectionIndex + "], itemIndex = [" + itemIndex + "]");
+		Section s = sections.get(sectionIndex);
+		s.items.remove(itemIndex);
+		notifySectionItemRemoved(sectionIndex, itemIndex);
+	}
+
+	void onCloneItem(int sectionIndex, int itemIndex) {
+		Log.d(TAG, "onCloneItem() called with: " + "sectionIndex = [" + sectionIndex + "], itemIndex = [" + itemIndex + "]");
+		Section s = sections.get(sectionIndex);
+		s.duplicateItem(itemIndex);
+		notifySectionItemInserted(sectionIndex, itemIndex + 1);
+	}
+
+	@Override
+	public int getNumberOfSections() {
+		return sections.size();
+	}
+
+	@Override
+	public int getNumberOfItemsInSection(int sectionIndex) {
+		return sections.get(sectionIndex).items.size();
+	}
+
+	@Override
+	public boolean doesSectionHaveHeader(int sectionIndex) {
+		return !TextUtils.isEmpty(sections.get(sectionIndex).header);
+	}
+
+	@Override
+	public boolean doesSectionHaveFooter(int sectionIndex) {
+		return !TextUtils.isEmpty(sections.get(sectionIndex).footer);
+	}
+
+	@Override
+	public ItemViewHolder onCreateItemViewHolder(ViewGroup parent) {
+		LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+		View v = inflater.inflate(R.layout.list_item_simple, parent, false);
+		return new ItemViewHolder(v);
+	}
+
+	@Override
+	public HeaderViewHolder onCreateHeaderViewHolder(ViewGroup parent) {
+		LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+		View v = inflater.inflate(R.layout.list_item_header_simple, parent, false);
+		return new HeaderViewHolder(v);
+	}
+
+	@Override
+	public FooterViewHolder onCreateFooterViewHolder(ViewGroup parent) {
+		LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+		View v = inflater.inflate(R.layout.list_item_footer_simple, parent, false);
+		return new FooterViewHolder(v);
+	}
+
+	@SuppressLint("SetTextI18n")
+	@Override
+	public void onBindItemViewHolder(SectioningAdapter.ItemViewHolder viewHolder, int sectionIndex, int itemIndex) {
+		Section s = sections.get(sectionIndex);
+		ItemViewHolder ivh = (ItemViewHolder) viewHolder;
+		ivh.textView.setText(s.items.get(itemIndex));
+		ivh.adapterPositionTextView.setText(Integer.toString(getAdapterPositionForSectionItem(sectionIndex, itemIndex)));
+	}
+
+	@SuppressLint("SetTextI18n")
+	@Override
+	public void onBindHeaderViewHolder(SectioningAdapter.HeaderViewHolder viewHolder, int sectionIndex) {
+		Section s = sections.get(sectionIndex);
+		HeaderViewHolder hvh = (HeaderViewHolder) viewHolder;
+		hvh.textView.setText(s.header);
+		hvh.adapterPositionTextView.setText(Integer.toString(getAdapterPositionForSectionHeader(sectionIndex)));
+
+		if (useDebugColoration) {
+			viewHolder.itemView.setBackgroundColor(0x55FF9999);
+		}
+	}
+
+	@Override
+	public void onBindGhostHeaderViewHolder(SectioningAdapter.GhostHeaderViewHolder viewHolder, int sectionIndex) {
+		if (useDebugColoration) {
+			viewHolder.itemView.setBackgroundColor(0xFF9999FF);
+		}
+	}
+
+	@SuppressLint("SetTextI18n")
+	@Override
+	public void onBindFooterViewHolder(SectioningAdapter.FooterViewHolder viewHolder, int sectionIndex) {
+		Section s = sections.get(sectionIndex);
+		FooterViewHolder fvh = (FooterViewHolder) viewHolder;
+		fvh.textView.setText(s.footer);
+		fvh.adapterPositionTextView.setText(Integer.toString(getAdapterPositionForSectionFooter(sectionIndex)));
+	}
+
+}
