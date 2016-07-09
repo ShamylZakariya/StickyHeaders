e58c32774df4672601d019e76d15eca201c0e3b4
Shamyl Zakariya <shamyl@gmail.com>
2016-07-09 12:58:18 -0700

    Give EndlessScrollDemo its own empty menu

--- app/src/main/java/org/zakariya/stickyheadersapp/ui/EndlessScrollDemoActivity.java
+++ app/src/main/java/org/zakariya/stickyheadersapp/ui/EndlessScrollDemoActivity.java
@@ -2,7 +2,11 @@ package org.zakariya.stickyheadersapp.ui;
 
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import org.zakariya.stickyheadersapp.R;
 import org.zakariya.stickyheadersapp.adapters.SimpleDemoAdapter;
 
 /**
@@ -17,4 +21,20 @@ public class EndlessScrollDemoActivity extends DemoActivity {
 		recyclerView.setLayoutManager(new LinearLayoutManager(this));
 		recyclerView.setAdapter(new SimpleDemoAdapter(5, 5, false, false, false));
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.menu_endless_scroll_demo, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
 }
